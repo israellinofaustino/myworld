b3546afd4de1326e221cf12e2f075ff5693adb7d
israellinofaustino <israellinofaustino@hotmail.com>
2024-06-02 21:19:31 +0100

    Django QuerySets

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -28,11 +28,20 @@ def details(request, id):
 
 def testing(request):
     mymembers = Member.objects.all().values()
+    membersfirstname = Member.objects.values_list('firstname')
+    user1 = Member.objects.filter(id='2').values()
+    startwith = Member.objects.filter(firstname__startswith='L').values()
+    order = Member.objects.all().order_by('firstname').values()
     template = loader.get_template("template.html")
     context = {
+        'order': order,
+        'startwith': startwith,
+        'user1': user1,
+        'membersfirstname': membersfirstname,
         'mymembers': mymembers,
         'x': 'Volvo',
         'y': 'Ford',
         'z': 'Fiat',
+        'emptytestobject': [],
     }
     return HttpResponse(template.render(context, request)) #create an object named context and fill it with data, and send it as the first parameter in the template.render() function
